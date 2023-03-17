b41b53cc5adfc62b629569daa91483000afc8389
WaShindeiru <fdata@student.agh.edu.pl>
2023-03-17 18:07:55 +0100

    First one completed, many others left

---
+++ src/com/example/solutions/Solution2.java
@@ -0,0 +1,12 @@
+package com.example.solutions;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+    }
+
+}
