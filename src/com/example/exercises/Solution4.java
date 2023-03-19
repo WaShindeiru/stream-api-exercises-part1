378c947aaab2cee33037e00627962253476ecd1c
WaShindeiru <franekdata@gmail.com>
2023-03-19 22:46:39 +0100

    4

---
+++ src/com/example/exercises/Solution4.java
@@ -0,0 +1,27 @@
+package com.example.exercises;
+
+import com.example.dao.InMemoryWorldDao;
+import com.example.dao.WorldDao;
+import com.example.domain.City;
+
+import java.util.Comparator;
+
+//Find the highest populated capital city
+public class Solution4 {
+    private static InMemoryWorldDao provider = InMemoryWorldDao.getInstance();
+
+    public static void main(String[] args) {
+        var countries = provider.findAllCountries();
+
+        var city = countries
+                .stream()
+                .map((country) -> country.getCities().stream().anyMatch((city1) -> city1.getId() == country.getCapital()))
+                .max(new Comparator<City>() {
+                    @Override
+                    public int compare(City city, City t1) {
+                        return city.getPopulation() - t1.getPopulation();
+                    }
+                });
+        System.out.println(city);
+    }
+}
